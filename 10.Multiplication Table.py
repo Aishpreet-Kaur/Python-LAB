be2c73d3ee04822468508361e93710c50eadc2b7
Aishpreet Kaur <aishpreet213@gmail.com>
2024-09-14 01:01:53 +0530

    Delete 10.Multiplication Table.py

--- 10.Multiplication Table.py
+++
@@ -1,7 +0,0 @@
-def multiplication_table(number):
-    for i in range(1, 11):
-        result = number * i
-        print(f"{number} x {i} = {result}")
-
-num = int(input("Enter the number:"))
-multiplication_table(num)
