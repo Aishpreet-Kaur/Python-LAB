a0f0ba4050a75fe82193b247de98839f1d224f12
Aishpreet Kaur <aishpreet213@gmail.com>
2024-09-27 00:17:25 +0530

    Create 4.19. String.py

---
+++ LAB-4/4.19. String.py
@@ -0,0 +1,10 @@
+#replacing , by . in the string
+X = "India.is.my.country"
+result = X.replace('.', ',')
+print(result)
+
+#removing dot(.) from the string
+Y = "M.A.N.I.P.A.L"
+result = Y.replace('.', '')
+print(result)
+
