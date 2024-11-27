094b5d2aba0c19600a9ec0ae4b993095e077b2d2
Aishpreet Kaur <aishpreet213@gmail.com>
2024-11-27 23:03:24 +0530

    Create 7.BinaryTree.py

---
+++ LAB - 7/7.BinaryTree.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, current, value):
+        if value < current.value:
+            if current.left is None:
+                current.left = Node(value)
+            else:
+                self._insert(current.left, value)
+        else:
+            if current.right is None:
+                current.right = Node(value)
+            else:
+                self._insert(current.right, value)
+
+    def inorder(self, node=None, values=[]):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.inorder(node.left, values)
+        values.append(node.value)
+        if node.right:
+            self.inorder(node.right, values)
+        return values
+
+tree = BinaryTree()
+tree.insert(50)
+tree.insert(30)
+tree.insert(70)
+tree.insert(20)
+tree.insert(40)
+tree.insert(60)
+tree.insert(80)
+
+print("Inorder Traversal:", tree.inorder())
